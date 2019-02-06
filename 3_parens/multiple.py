9e2d532e391114f10f33b0dc6bd17703451e199b
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-02-06 13:49:18 -0500

    Remove solutions

--- 3_parens/multiple.py
+++ 3_parens/multiple.py
@@ -1,12 +1,2 @@
 def solve(x):
-    stack = []
-    pairs = {left: right for left, right in '() {} []'.split()}
-    for c in x:
-        if c in pairs:
-            stack.append(c)
-        else:
-            if stack and pairs[stack[-1]] == c:
-                stack.pop()
-            else:
-                return False
-    return not stack
+    return False
--- 3_parens/single.py
+++ 3_parens/single.py
@@ -1,11 +1,2 @@
 def solve(x):
-    lefts = 0
-    for c in x:
-        if c == '(':
-            lefts += 1
-        if c == ')':
-            if lefts:
-                lefts -= 1
-            else:
-                return False
-    return lefts == 0
+    return False
